6aaaff727b9d109fd17602105925e7b131e82ab2
Shiva Krishna <shivakrishna6032@gmail.com>
2023-12-17 10:28:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 import { Box } from '@mui/material';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
 import Recipes from './components/Recipes';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename='/EcoChef'>
       <Navbar/>
       <Routes>
-        <Route path='/' element={ <Home/>}/>
+        <Route path='/EcoChef' element={ <Home/>}/>
         <Route path='/recipes' element={ <Recipes/>}/>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
